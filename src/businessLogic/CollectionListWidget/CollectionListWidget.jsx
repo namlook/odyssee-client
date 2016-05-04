9159a7b8c2de0d708dc324f3deea9b0b8f91ab84
namlook <nicolas.clairon@elkorado.com>
2016-05-04 17:08:11 +0200

    placeholder when no record found

--- src/businessLogic/CollectionListWidget/CollectionListWidget.jsx
+++ src/businessLogic/CollectionListWidget/CollectionListWidget.jsx
@@ -7,27 +7,34 @@ const CollectionListWidget = (props) => {
   const records = storeState[link.collection.from].get(link.collection.to);
 
   const displayRecord = (record) => (
-    properties.map((property) => <p key={property}>{record[property]}</p>)
+    properties.map((property) => <td key={property}>{record[property]}</td>)
+  );
+
+  const table = records.count() ? (
+    <table style={{ border: 0 }} className={`ui striped table`}>
+      <thead>
+        <tr> {properties.map((name) => <th key={name}> {name} </th>)} </tr>
+      </thead>
+      <tbody>
+        {records.map((record) => (
+          <tr key={record._id}> {displayRecord(record)} </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : (
+    <div className="ui center aligned segment"> {"no records found"} </div>
   );
 
   return (
-    <CardWidget
-      _name="collection-list"
-      {...props}
-    >
-      <div className="ui segment">
-        <ul>
-          {records.map((record) => (
-            <li key={record._id}> {displayRecord(record)} </li>
-          ))}
-        </ul>
-      </div>
+    <CardWidget _name="collection-list" {...props}>
+      {table}
     </CardWidget>
   );
 };
 
 CollectionListWidget.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
   properties: PropTypes.array.isRequired,
   storeState: PropTypes.object.isRequired,
   link: PropTypes.object.isRequired,
