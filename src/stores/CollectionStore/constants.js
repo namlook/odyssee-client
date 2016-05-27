e2c3e4ebc75b6a9ac5c8deaca8f389c9ca0c67eb
namlook <nicolas.clairon@elkorado.com>
2016-05-27 15:27:11 +0200

    remove CollectionStore

--- src/stores/CollectionStore/constants.js
+++
@@ -1,4 +0,0 @@
-export const ADD_RECORD = 'CollectionStore.ADD_RECORD';
-export const UPDATE_RECORD = 'CollectionStore.UPDATE_RECORD';
-export const DELETE_RECORD = 'CollectionStore.DELETE_RECORD';
-export const DELETE_ALL = 'CollectionStore.DELETE_ALL';
--- src/stores/CollectionStore/index.js
+++
@@ -1,4 +0,0 @@
-
-export * as actions from './actions';
-export * as constants from './constants';
-export reducer from './reducer';
